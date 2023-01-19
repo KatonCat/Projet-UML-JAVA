97878034200bcebcdb6c8c86711052f0a2466cbe
Miauleur <anuar.akkar@gmail.com>
2023-01-19 16:48:34 +0100

    THread List and maintest a changer

--- src/main/java/Clavardage/ThreadList.java
+++ src/main/java/Clavardage/ThreadList.java
@@ -7,7 +7,11 @@ public class ThreadList {
     private List<ServeurTCP.ClientHandler> clientHandler = new ArrayList<>();
     public void addThread(ServeurTCP.ClientHandler t){clientHandler.add(t);}
     public void delThread(ServeurTCP.ClientHandler t){clientHandler.remove(t);}
-    //public int findThread(String s){return  thread.indexOf(s);}
-    public List<ServeurTCP.ClientHandler> getThread(){return clientHandler;}
+    public int findThread(String s){return  clientHandler.indexOf(s);}
+    public ServeurTCP.ClientHandler getThread(int i){return clientHandler.get(i);}
+    public int lengthListe(){
+        return clientHandler.size();
+    }
+    public List<ServeurTCP.ClientHandler> getList(){return clientHandler;}
 }
 
--- src/main/java/mainTest.java
+++ src/main/java/mainTest.java
@@ -1,17 +1,33 @@
 import Clavardage.MultiServeurTCP;
+import Clavardage.ServeurTCP;
+import Clavardage.clientTCP;
 import Connexion.Connexion;
 import Connexion.Ecoute;
 import Clavardage.StartSession;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class mainTest {
     public static void main(String args[]) throws IOException, ClassNotFoundException, UnknownHostException, InterruptedException {
        // new Ecoute().start();
        // new Connexion().verifyId();
-        MultiServeurTCP Server = new MultiServeurTCP(1769);
+        ServeurTCP Server = new ServeurTCP(1769);
         Server.start();
+        //int index = ServeurTCP.threadList.findThread("127.0.0.1");
+        Scanner entreeClavier = new Scanner(System.in);
+        while(ServeurTCP.threadList.lengthListe()==0)
+        {}
+        ServeurTCP.ClientHandler thread = ServeurTCP.threadList.getThread(0);
+        clientTCP client = thread.getClient();
+
+        while(true){
+            String msg = entreeClavier.nextLine();
+            client.sendMessage(msg);
+        }
+
+        };
        // StartSession.StartSession(InetAddress.getByName("127.0.0.1"));
-    }
 }
+
